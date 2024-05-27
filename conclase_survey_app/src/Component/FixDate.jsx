228d1e39996da42fcbc6a92b5f8ddf3454a493d4
David Ohiosimuan Ohimai <daveohimai@gmail.com>
2024-05-27 20:42:52 +0100

    Revert "feature/Add 3 modals"

--- conclase_survey_app/src/Component/FixDate.jsx
+++ conclase_survey_app/src/Component/FixDate.jsx
@@ -1,20 +1,7 @@
-import { React, useState } from 'react';
-import "../Styles/Modals/FixDate.css";
+import React from 'react';
+import "../Styles/Dashboard/FixDate.css";
 
-const ModalDate = ({ onClose }) => {
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-
-  const handleCreate = () => {
-    // Handle form submission logic here
-    console.log("Start Date:", startDate);
-    console.log("End Date:", endDate);
-    console.log("Start Time:", startTime);
-    console.log("End Time:", endTime);
-    onClose();
-  };
+const ModalDate = () => {
   return (
     <div className="modal2">
       <div className="hover-state">
@@ -26,26 +13,14 @@ const ModalDate = ({ onClose }) => {
         <div className="dateframe">
           <div className="dateframe-1">
             <span className="from">From</span>
-            {/* <span className="date-2">20.03.2024</span> */}
-            <input
-              type="date"
-              className="date-2"
-              value={startDate}
-              onChange={(e) => setStartDate(e.target.value)}
-            />
+            <span className="date-2">20.03.2024</span>
           </div>
           <div className="datesolar-calendar-linear" />
         </div>
         <div className="dateframe-3">
           <div className="dateframe-4">
             <span className="to">To</span>
-            {/* <span className="date-5">20.03.2024</span> */}
-            <input
-              type="date"
-              className="date-5"
-              value={endDate}
-              onChange={(e) => setEndDate(e.target.value)}
-            />
+            <span className="date-5">20.03.2024</span>
           </div>
           <div className="solar-calendar-linear-6" />
         </div>
@@ -55,32 +30,19 @@ const ModalDate = ({ onClose }) => {
         <div className="dateframe-7">
           <div className="dateframe-8">
             <span className="start">Start</span>
-            {/* <span className="time-9">9:00</span> */}
-
-            <input
-              type="time"
-              className="time-9"
-              value={startTime}
-              onChange={(e) => setStartTime(e.target.value)}
-            />
+            <span className="time-9">9:00</span>
           </div>
         </div>
         <div className="dateframe-a">
           <div className="dateframe-b">
             <span className="end">End</span>
-            {/* <span className="time-c">20:00</span> */}
-            <input
-              type="time"
-              className="time-c"
-              value={endTime}
-              onChange={(e) => setEndTime(e.target.value)}
-            />
+            <span className="time-c">20:00</span>
           </div>
         </div>
       </div>
       <div className="dateframe-d">
         <button className="login-button">
-          <span className="create"onClick={handleCreate}>Create</span>
+          <span className="create">Create</span>
         </button>
         <div className="dateregister">
           <span className="cancel">Cancel</span>
@@ -90,4 +52,4 @@ const ModalDate = ({ onClose }) => {
   );
 }
 
-export default ModalDate;
+export default ModalDate
